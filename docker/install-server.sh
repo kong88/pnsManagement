87dd665a4ea0bc4b5c0da217eb2bbb5312bddbda
FrancescoUK <francesco.lodolo.uk@gmail.com>
2019-03-23 21:45:56 +0000

    Reorg Docker files to docker folder, create install scripts

---
+++ docker/install-server.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ ! -d "data/ospos/app/application" ]; then
+  mkdir data/ospos/app/application
+  sudo mount --bind ../application/ data/ospos/app/application/
+fi
+if [ ! -d "data/ospos/app/public" ]; then
+  mkdir data/ospos/app/public
+  sudo mount --bind ../public/ data/ospos/app/public/
+fi
+if [ ! -d "data/ospos/app/vendor" ]; then
+  mkdir data/ospos/app/vendor
+  sudo mount --bind ../vendor/ data/ospos/app/vendor/
+fi
+
+cp ../*.csv data/ospos/app/
+
+if [ ! -d "data/database/db" ]; then
+  mkdir data/database/db
+  sudo mount --bind ../database/ data/database/db/
+fi
+
+docker-compose build
+
+/bin/bash ./init-letsencrypt.sh
